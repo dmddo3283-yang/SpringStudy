594117372e7ad13a9ab02591f57f2a8622fa7a12
Yang jeongwoo <25_39@bssm.hs.kr>
2026-04-05 03:40:32 +0900

    Qulifier를 통한 의존성 주입

--- src/main/java/com/example/springstudy/discount/FixDiscountPolicy.java
+++ src/main/java/com/example/springstudy/discount/FixDiscountPolicy.java
@@ -2,7 +2,11 @@ package com.example.springstudy.discount;
 
 import com.example.springstudy.member.Grade;
 import com.example.springstudy.member.Member;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
 
+@Component
+@Qualifier("fixDiscountPolicy")
 public class FixDiscountPolicy implements DiscountPolicy {
     private int discountFixAmount = 1000;
 
--- src/main/java/com/example/springstudy/discount/RateDiscountPolicy.java
+++ src/main/java/com/example/springstudy/discount/RateDiscountPolicy.java
@@ -2,9 +2,12 @@ package com.example.springstudy.discount;
 
 import com.example.springstudy.member.Grade;
 import com.example.springstudy.member.Member;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Component
+@Qualifier("mainDiscountPolicy")
 public class RateDiscountPolicy implements DiscountPolicy {
     private int discountPercent = 10;
 
