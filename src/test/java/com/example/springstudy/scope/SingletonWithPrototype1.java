af3e0258c33fe93b1319db0e45d78d9c007ac79a
Yang jeongwoo <25_39@bssm.hs.kr>
2026-04-07 17:26:15 +0900

    prototype 문제점

---
+++ src/test/java/com/example/springstudy/scope/SingletonWithPrototype1.java
@@ -0,0 +1,45 @@
+package com.example.springstudy.scope;
+
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Scope;
+
+public class SingletonWithPrototype1 {
+
+    @Test
+    void prototypeFind() {
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(PrototypeBean.class);
+        PrototypeBean bean1 = ac.getBean(PrototypeBean.class);
+        bean1.addCount();
+        Assertions.assertThat(bean1.getCount()).isEqualTo(1);
+
+        PrototypeBean bean2 = ac.getBean(PrototypeBean.class);
+        bean2.addCount();
+        Assertions.assertThat(bean2.getCount()).isEqualTo(1);
+    }
+
+    @Scope("prototype")
+    static class PrototypeBean {
+        private int count = 0;
+
+        public void addCount() {
+            count++;
+        }
+        
+        public int getCount() {
+            return count;
+        }
+        
+        @PostConstruct
+        public void init() {
+            System.out.println("PrototypeBean.init" + this);
+        }
+        @PreDestroy
+        public void destroy() {
+            System.out.println("PrototypeBean.destroy" + this);
+        }
+    }
+}
