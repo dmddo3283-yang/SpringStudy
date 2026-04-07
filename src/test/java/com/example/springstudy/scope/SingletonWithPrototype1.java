c2cacce440dde6d19ae913fe6aa7e06de03828b3
Yang jeongwoo <25_39@bssm.hs.kr>
2026-04-07 17:38:22 +0900

    prototype 사용 시 주의하여야할 점

--- src/test/java/com/example/springstudy/scope/SingletonWithPrototype1.java
+++ src/test/java/com/example/springstudy/scope/SingletonWithPrototype1.java
@@ -4,6 +4,7 @@ import jakarta.annotation.PostConstruct;
 import jakarta.annotation.PreDestroy;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Scope;
 
@@ -19,6 +20,35 @@ public class SingletonWithPrototype1 {
         PrototypeBean bean2 = ac.getBean(PrototypeBean.class);
         bean2.addCount();
         Assertions.assertThat(bean2.getCount()).isEqualTo(1);
+    }
+
+    @Test
+    void singletonClientUserPrototype() {
+        AnnotationConfigApplicationContext ac =
+                new AnnotationConfigApplicationContext(ClientBean.class, PrototypeBean.class);
+
+        ClientBean bean1 = ac.getBean(ClientBean.class);
+        int count1 = bean1.logic();
+        Assertions.assertThat(count1).isEqualTo(1);
+
+        ClientBean bean2 = ac.getBean(ClientBean.class);
+        int count2 = bean2.logic();
+        Assertions.assertThat(count2).isEqualTo(2);
+    }
+
+    @Scope("singleton")
+    static class ClientBean {
+        private final PrototypeBean prototypeBean;
+
+        public ClientBean(PrototypeBean prototypeBean) {
+            this.prototypeBean = prototypeBean;
+        }
+
+        public int logic() {
+            prototypeBean.addCount();
+            int count = prototypeBean.getCount();
+            return count;
+        }
     }
 
     @Scope("prototype")
