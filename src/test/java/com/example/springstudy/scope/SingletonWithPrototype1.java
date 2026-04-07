8d5fbde43dbc96a6b68da03888be49420b5de4aa
Yang jeongwoo <25_39@bssm.hs.kr>
2026-04-07 17:40:16 +0900

    앞 코드에서 prototype의 성질을 유지하도록 하는 법

--- src/test/java/com/example/springstudy/scope/SingletonWithPrototype1.java
+++ src/test/java/com/example/springstudy/scope/SingletonWithPrototype1.java
@@ -5,6 +5,7 @@ import jakarta.annotation.PreDestroy;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Scope;
 
@@ -38,13 +39,17 @@ public class SingletonWithPrototype1 {
 
     @Scope("singleton")
     static class ClientBean {
-        private final PrototypeBean prototypeBean;
+//        private final PrototypeBean prototypeBean;
 
-        public ClientBean(PrototypeBean prototypeBean) {
-            this.prototypeBean = prototypeBean;
-        }
+        @Autowired
+        ApplicationContext applicationContext;
+
+//        public ClientBean(PrototypeBean prototypeBean) {
+//            this.prototypeBean = prototypeBean;
+//        }
 
         public int logic() {
+            PrototypeBean prototypeBean = applicationContext.getBean(PrototypeBean.class);
             prototypeBean.addCount();
             int count = prototypeBean.getCount();
             return count;
