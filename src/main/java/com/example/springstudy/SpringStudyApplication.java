c44ad7b291bd7468487a307d103ff66bc104639a
Yang jeongwoo <25_39@bssm.hs.kr>
2026-04-08 16:49:25 +0900

    request 스코프 예제 작성

--- src/main/java/com/example/springstudy/SpringStudyApplication.java
+++ src/main/java/com/example/springstudy/SpringStudyApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringStudyApplication {
+public class  SpringStudyApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringStudyApplication.class, args);
---
+++ src/main/java/com/example/springstudy/web/LogDemoService.java
@@ -0,0 +1,15 @@
+package com.example.springstudy.web;
+
+import com.example.springstudy.common.MyLogger;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class LogDemoService {
+    private final MyLogger myLogger;
+
+    public void logic(String id) {
+        myLogger.log("service id = " + id);
+    }
+}
