952b32f5a4f807383dfa32006a1d35f9cff04ab5
Yang jeongwoo <yangjeongwoo3283@gmail.com>
2026-04-12 01:11:24 +0900

    request header 생성

--- spring-study-second/src/main/java/hello/survlet/SpringStudySecondApplication.java
+++ spring-study-second/src/main/java/hello/survlet/SpringStudySecondApplication.java
@@ -2,7 +2,10 @@ package hello.survlet;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.server.servlet.context.ServletComponentScan;
 
+
+@ServletComponentScan
 @SpringBootApplication
 public class SpringStudySecondApplication {
 
