17150f017a451e500cf153029468de4241dfbcfb
Yang jeongwoo <yangjeongwoo3283@gmail.com>
2026-04-11 19:22:08 +0900

    hello servlet 생성

---
+++ spring-study-second/src/main/java/hello/survlet/SpringStudySecondApplication.java
@@ -0,0 +1,13 @@
+package hello.survlet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringStudySecondApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringStudySecondApplication.class, args);
+    }
+
+}
