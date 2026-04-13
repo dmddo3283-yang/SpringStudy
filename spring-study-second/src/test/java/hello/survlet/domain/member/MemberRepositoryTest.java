8b6a23d1613627cc8852b1653d27154ce543ff99
Yang jeongwoo <yangjeongwoo3283@gmail.com>
2026-04-14 01:11:07 +0900

    test code created

---
+++ spring-study-second/src/test/java/hello/survlet/domain/member/MemberRepositoryTest.java
@@ -0,0 +1,43 @@
+package hello.survlet.domain.member;
+
+import hello.survlet.basic.domain.member.Member;
+import hello.survlet.basic.domain.member.MemberRepository;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+public class MemberRepositoryTest {
+
+    MemberRepository memberRepository = MemberRepository.getInstance();
+
+    @AfterEach
+    void afterEach() {
+        memberRepository.clearStore();
+    }
+
+    @Test
+    void save() {
+        Member hello = new Member("hello", 20);
+
+        Member savedMember = memberRepository.save(hello);
+
+        Member findMember = memberRepository.findById(savedMember.getId());
+        Assertions.assertThat(savedMember).isEqualTo(findMember);
+    }
+
+    @Test
+    void findAll() {
+        Member member1 = new Member("member1", 20);
+        Member member2 = new Member("member2", 20);
+
+        memberRepository.save(member1);
+        memberRepository.save(member2);
+
+        List<Member> memberList = memberRepository.findAll();
+
+        Assertions.assertThat(memberList.size()).isEqualTo(2);
+        Assertions.assertThat(memberList).contains(member1, member2);
+    }
+}
