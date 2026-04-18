b8d9fd26b6c09d3df28ac840553adb6ec95002c9
Yang jeongwoo <yangjeongwoo3283@gmail.com>
2026-04-18 20:47:54 +0900

    Controller v1 구현체 MemberListController V1 생성

--- spring-study-second/src/main/java/hello/survlet/web/frontcontroller/v1/controller/MemberListControllerV1.java
+++ spring-study-second/src/main/java/hello/survlet/web/frontcontroller/v1/controller/MemberListControllerV1.java
@@ -9,21 +9,18 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
+import java.util.List;
 
-public class MemberSaveControllerV1 implements ControllerV1 {
+public class MemberListControllerV1 implements ControllerV1 {
     private MemberRepository memberRepository = MemberRepository.getInstance();
 
     @Override
     public void process(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        String username = request.getParameter("username");
-        int age = Integer.parseInt(request.getParameter("age"));
+        List<Member> members = memberRepository.findAll();
 
-        Member member = new Member(username, age);
-        memberRepository.save(member);
+        request.setAttribute("members", members);
 
-        request.setAttribute("member", member);
-
-        String viewPath = "/WEB-INF/views/save-result.jsp";
+        String viewPath = "/WEB-INF/views/members.jsp";
         RequestDispatcher dispatcher = request.getRequestDispatcher(viewPath);
         dispatcher.forward(request, response);
     }
